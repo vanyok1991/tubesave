9c47d8313dcbaa62e4d6cd447b1ca268d39327eb
ibielko <ivan.bielko@onseo.biz>
2018-01-24 12:05:10 +0200

    added bottom bunner

--- UTubeSave/MainActivity.cs
+++ UTubeSave/MainActivity.cs
@@ -4,21 +4,37 @@ using Android.OS;
 using System.Collections.Generic;
 using YoutubeExtractor;
 using System.Linq;
-using System.Net;
-using System;
 using System.IO;
+using Android.Gms.Ads;
 
 namespace UTubeSave
 {
     [Activity(Label = "UTubeSave", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        string appId = "ca-app-pub-6653353220256677~5913709845";
+        string bunnerId = "ca-app-pub-6653353220256677/9117978752";
+        string showId = "ca-app-pub-6653353220256677/4139972485";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            MobileAds.Initialize(this, appId);
+
+            var mAdView = FindViewById<AdView>(Resource.Id.adView);
+            var adRequest = new AdRequest.Builder().Build();
+            mAdView.LoadAd(adRequest);
+
+            //var mInterstitialAd = new InterstitialAd(this);
+            //mInterstitialAd.AdUnitId = GetString(Resource.String.test_interstitial_ad_unit_id);
+
+            //mInterstitialAd.AdListener = new AdListener();
+
+
 
             string link = "https://www.youtube.com/watch?v=DHHY8m3rEzU";
             IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
@@ -51,7 +67,7 @@ namespace UTubeSave
 
             videoView.SetVideoURI(uri);
 
-            videoView.Start();
+            //videoView.Start();
         }
     }
 }
